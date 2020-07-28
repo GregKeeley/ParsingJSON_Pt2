409c77c5c6989dee0d9514435a43ba5ae9cf5826
Greg Keeley <GregoryKeeley@pursuit.org>
2020-07-28 13:04:58 -0400

    Added decoding JSON data with multiple keys

--- PlayingWithJSON.playground/Pages/Parsing Multiple Arrays.xcplaygroundpage/Contents.swift
+++ PlayingWithJSON.playground/Pages/Parsing Multiple Arrays.xcplaygroundpage/Contents.swift
@@ -92,5 +92,29 @@ let json = """
         }
     }
 }
-"""
+""".data(using: .utf8)!
 
+// Models
+
+struct Strain: Decodable {
+    let id: Int
+    let race: String
+    let flavors: [String]
+    let effects: [String: [String]]
+}
+
+do {
+    let dictionary = try JSONDecoder().decode([String: Strain].self, from: json)
+    // use a for-loop to create [Strain] or use map {}
+    var strains = [Strain]()
+    for (_, value) in dictionary {
+        let strain = Strain(id: value.id,
+                            race: value.race,
+                            flavors: value.flavors,
+                            effects: value.effects)
+        strains.append(strain)
+    }
+    dump(strains)
+} catch {
+   print(error)
+}
