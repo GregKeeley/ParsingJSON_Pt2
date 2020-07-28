d28a843bf25bfdcc0229c47e704262b024bae761
Greg Keeley <GregoryKeeley@pursuit.org>
2020-07-28 12:24:05 -0400

    Updated project with JSON data

--- PlayingWithJSON.playground/Contents.swift
+++ PlayingWithJSON.playground/Contents.swift
@@ -1,3 +1,36 @@
-import UIKit
+import Foundation
 
 // Parsing Dictionary
+let json = """
+{
+ "results": [
+   {
+     "firstName": "John",
+     "lastName": "Appleseed"
+   },
+  {
+    "firstName": "Alex",
+    "lastName": "Paul"
+  }
+ ]
+}
+""".data(using: .utf8)!
+
+struct ResultsWrapper: Codable {
+    let results: [Contact]
+}
+
+struct Contact: Codable {
+    let firstName: String
+    let lastName: String
+}
+
+// Decoding the JSON
+
+do {
+    let dictionary = try JSONDecoder().decode(ResultsWrapper.self, from: json)
+    let contacts = dictionary.results
+    dump(contacts)
+} catch {
+    print("decoding error: \(error)")
+}
